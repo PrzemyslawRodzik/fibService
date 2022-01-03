6b1453f9c1f85d5b1a557401a69b457358412540
Przemysław Rodzik <przemyslawrodzik@gmail.com>
2022-01-03 18:47:48 +0100

    add readme links to docs page

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => {
   return (
     <div>
-      Im some other page!
+      <a href="https://github.com/PrzemyslawRodzik/fibService">
+        README GitHub.com
+      </a>
+      <br />
+      <a href="https://gitlab.com/PrzemyslawRodzik/fibService">
+        README GitLab.com
+      </a>
       <Link to="/">Go back home</Link>
     </div>
   );
